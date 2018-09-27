af5dad2718431a90bf6a282442668a0163faf6b7
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-09-27 03:00:15 -0700

    sorting

--- semver_test.go
+++ semver_test.go
@@ -2,6 +2,7 @@ package semver
 
 import (
 	"encoding/json"
+	"sort"
 	"testing"
 
 	. "github.com/franela/goblin"
@@ -23,14 +24,24 @@ func renderJSON(v *Version) string {
 	return string(bytes)
 }
 
+func p(s string) *Version {
+	return MustParse(s)
+}
 func Test(t *testing.T) {
 	g := Goblin(t)
 	g.Describe("Parse", func() {
 		g.It("parses", func() {
-			g.Assert(MustParse("0.0.0")).Equal(&Version{Major: 0, Minor: 0, Patch: 0})
-			g.Assert(MustParse("1.2.3")).Equal(&Version{Major: 1, Minor: 2, Patch: 3})
+			g.Assert(p("0.0.0")).Equal(&Version{Major: 0, Minor: 0, Patch: 0})
+			g.Assert(p("1.2.3")).Equal(&Version{Major: 1, Minor: 2, Patch: 3})
 			g.Assert(parseJSON(`{"version": "1.2.3"}`).Version).Equal(&Version{Major: 1, Minor: 2, Patch: 3})
 			g.Assert(renderJSON(&Version{Major: 1, Minor: 2, Patch: 3})).Equal(`{"version":"1.2.3"}`)
+
+			versions := Versions{p("1.2.3"), p("2.0.0"), p("1.4.2"), p("1.2.4")}
+			sort.Sort(versions)
+			g.Assert(versions[0]).Equal(p("1.2.3"))
+			g.Assert(versions[1]).Equal(p("1.2.4"))
+			g.Assert(versions[2]).Equal(p("1.4.2"))
+			g.Assert(versions[3]).Equal(p("2.0.0"))
 		})
 	})
 }
--- version.go
+++ version.go
@@ -69,3 +69,54 @@ func (this *Version) UnmarshalJSON(b []byte) error {
 	this.Patch = v.Patch
 	return nil
 }
+
+func compare(a, b int) int {
+	if a < b {
+		return -1
+	}
+	if b < a {
+		return 1
+	}
+	return 0
+}
+
+func (a *Version) compareMajor(b *Version) int {
+	return compare(a.Major, b.Major)
+}
+
+func (a *Version) compareMinor(b *Version) int {
+	return compare(a.Minor, b.Minor)
+}
+
+func (a *Version) comparePatch(b *Version) int {
+	return compare(a.Patch, b.Patch)
+}
+func (a *Version) compare(b *Version) int {
+	var c int
+	c = a.compareMajor(b)
+	if c != 0 {
+		return c
+	}
+	c = a.compareMinor(b)
+	if c != 0 {
+		return c
+	}
+	return a.comparePatch(b)
+}
+
+// LT returns true is given version is less than this one
+func (a *Version) LT(b *Version) bool {
+	return a.compare(b) == -1
+}
+
+type Versions []*Version
+
+func (v Versions) Len() int {
+	return len(v)
+}
+func (v Versions) Less(a, b int) bool {
+	return v[a].LT(v[b])
+}
+func (v Versions) Swap(a, b int) {
+	v[a], v[b] = v[b], v[a]
+}
